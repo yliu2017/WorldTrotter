22ded41836e9e5793a93d58b0eee2c5a18d798f1
Tom Mac <yliu@wils326-vmxp-sl.highpoint.edu>
2017-01-30 17:14:31 -0500

    Chapter 3 Finished

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /* Practicing Code
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //to create a CGRect, need to initialize orgin.x,orgin.y,size.width,and size.hieight
+        let firstFrame = CGRect(x: 160,y: 240, width: 100, height: 150)
+        let firstView =  UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x:20, y:30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)  -->add secondView as a subview of view
+        firstView.addSubview(secondView)  //add secondView as a subview of firstView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    Practicing Code End */
+    
+    
 
 }
 
