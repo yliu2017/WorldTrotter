d60dd30b4f5e9ab19b0682a0284bfa6ddd5c272e
Tom Mac <yliu@Toms-iMac.local>
2017-02-01 20:35:10 -0500

    chapter 4 complete!

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,21 +8,96 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController
+class ConversionViewController: UIViewController, UITextFieldDelegate
 {
     @IBOutlet var celsiusLable: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        }else {
+            return nil
+        }
+    }
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField)
     {
         //celsiusLable.text = textField.text
-        if let text = textField.text, !text.isEmpty
-        {
-            celsiusLable.text = text
+        if let text = textField.text, let value = Double(text)        {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         }else
         {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer)
+    {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            //celsiusLable.text = "\(celsiusValue.value)"
+            celsiusLable.text =
+                numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        }else {
             celsiusLable.text = "???"
         }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //to set the initial value
+        updateCelsiusLabel()
+    }
+   
+    //Number Formatters
+    let  numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
+    //the UITextFieldDelegate protocol:
+    /*protocol UITextFieldDelegate: NSObjectProtocol {
+        optional func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool
+        optional func textFieldDidBeginEditing(_ textField: UITextField)
+        optional func textFieldShouldEndEditing(_ textField: UITextField) -> Bool
+        optional func textFieldDidEndEditing(_ textField: UITextField)
+        optional func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                                replacementString string: String) -> Bool
+        optional func textFieldShouldClear(_ textField: UITextField) -> Bool
+        optional func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    }
+    */
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        print("Current text: \(textField.text)")
+        print("Replacement text: \(string)")
+        
+        return true
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
